3b613bc79adba3acbb2495b8647e89259b645eff
taoxingyu <1396563360@qq.com>
2020-02-26 09:02:53 +0800

    提交

---
+++ haah/src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void onButton(ActionEvent actionEvent) {
+        System.out.println("hellow world");
+        while (true){}
+    }
+}
